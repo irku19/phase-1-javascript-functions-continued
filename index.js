33db6e582cc1acfce6d1de4608dd5a017dc09e67
İrem Kurt <iremkurt19@gmail.com>
2022-04-24 19:05:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+saturdayFun("bathe my dog");
+
+function mondayWork(duty='go to the office'){
+    return `This Monday, I will ${duty}.`
+}
+
+function wrapAdjective(result= '*'){
+    return function emphatic(result0 ="a hard worker"){
+        return `You are ${result}${result0}${result}!`
+    }
+
+}
+
+function wrapAdjective(result= "||"){
+    return function emphatic(result0 ="a dedicated programmer"){
+        return `You are ${result}${result0}${result}!`
+    }
+
+}
